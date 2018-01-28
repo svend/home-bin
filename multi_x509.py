4e9572f9725677b68d3db8db2e865835d556d131
Svend Sorensen <svend@svends.net>
2018-01-28 10:29:50 -0800

    Rename

---
+++ multi_x509.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import sys
+
+COMMAND = ['cat']
+
+
+def certs(iterator):
+    '''Yields certificates from iterator.'''
+    BEGIN = '-----BEGIN'
+    END = '-----END'
+
+    it = iter(iterator)
+    cert_lines = []
+    for line in it:
+        if line.startswith(BEGIN) or cert_lines:
+            cert_lines.append(line)
+            if line.startswith(END):
+                yield ''.join(cert_lines)
+                cert_lines = []
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        usage='%(prog)s [options] [command]',
+        description='''Read certificates from stdin and run command on each
+        one. Default command is `{}`. '''.format(' '.join(COMMAND)))
+    parser.add_argument(
+        '--after', help='string to print after each result')
+    return parser.parse_known_args()
+
+
+if __name__ == '__main__':
+    (args, command) = parse_args()
+
+    command = (command or COMMAND)
+    for cert in certs(sys.stdin):
+        subprocess.run(command, input=str.encode(cert))
+        if args.after is not None:
+            print(args.after)
